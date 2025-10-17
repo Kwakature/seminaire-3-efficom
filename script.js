88f8dc1923f00479e5224b1636a32615f4728a2d
kwakature <berton.arthur24@gmail.com>
2025-10-17 10:33:46 +0200

    section ckicable

--- script.js
+++ script.js
@@ -30,3 +30,16 @@ fetch('header.html')
             });
         });
     });
+
+document.getElementById('contact_mail').addEventListener('click', function() {
+    window.location.href = "contact.html";
+});
+document.getElementById('linke').addEventListener('click', function() {
+    window.location.href = "https://linkedin.com/in/arthur-berton-6b1200338";
+});
+document.getElementById('git').addEventListener('click', function() {
+    window.location.href = "https://github.com/Kwakature";
+});
+document.getElementById('projet_eff').addEventListener('click', function() {
+    window.location.href = "https://github.com/Kwakature/fil-rouge-EFFICOM";
+});
